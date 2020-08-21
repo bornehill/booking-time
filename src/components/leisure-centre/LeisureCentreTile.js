969d2f296b6f5caf359f5986be354be62e1eabf1
bornehill <ali.escamilla.navarro@gmail.com>
2020-08-20 20:33:29 -0500

    centre-tile styles

--- src/components/leisure-centre/LeisureCentreTile.js
+++ src/components/leisure-centre/LeisureCentreTile.js
@@ -5,11 +5,11 @@ const LeisureCentreTile = ({centre}) => {
       <div className="card centre-tile">
         <div className="card-body">
           <h5 className="card-title">{centre.name}</h5>
-          <div className="card-text"><span className="bold">Dirección: </span>{centre.address}</div>
-          <div className="card-text"><span className="bold">Email: </span>{centre.email}</div>
-          <div className="card-text"><span className="bold">Telefono: </span>{centre.phone}</div>
-          <div className="card-text"><span className="bold">Giro: </span>{centre.line}</div>
-          <div className="center"><a className="btn btn-basic">Info</a></div>
+          <div className="card-text"><span className="font-weight-bold">Dirección: </span>{centre.address}</div>
+          <div className="card-text"><span className="font-weight-bold">Email: </span>{centre.email}</div>
+          <div className="card-text"><span className="font-weight-bold">Telefono: </span>{centre.phone}</div>
+          <div className="card-text"><span className="font-weight-bold">Giro: </span>{centre.line}</div>
+          <div className="d-flex justify-content-center"><a className="btn btn-basic">Info</a></div>
         </div>
       </div>
     );
